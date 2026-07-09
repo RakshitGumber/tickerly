512d6632b9f42b37b911c403155f34629b419092
RakshitGumber <gumberrakshit@gmail.com>
2026-07-09 23:12:30 +0530

    added color theme for the frontend

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   const { setTicker } = useTicker((state) => state);
 
   return (
-    <nav className="bg-neutral-primary relative w-full z-20 top-0 inset-s-0 border-b border-default">
+    <nav className="bg-panel-color relative w-full z-20 top-0 inset-s-0 border-b border-border-color">
       <div className="max-w-6xl flex flex-wrap items-center justify-between mx-auto px-8 py-2">
         <a
           href="https://flowbite.com/"
@@ -66,7 +66,7 @@ export const Navbar = () => {
             <input
               type="text"
               id="input-group-1"
-              className="block w-full ps-9 pe-3 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand px-2.5 shadow-xs placeholder:text-body"
+              className="block w-full ps-9 pe-3 py-2.5  border border-border-color text-heading text-sm rounded-base focus:ring-brand focus:border-brand px-2.5 shadow-xs placeholder:text-body"
               placeholder="Search"
               onChange={(e) => setTicker(e.target.value)}
             />
---
+++ apps/web/src/components/RawReturns.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { fetchReturns } from "../api";
+import { useTicker } from "../store/useTicker";
+
+export function RawReturns() {
+  const { ticker } = useTicker((state) => state);
+  const [returns, setReturns] = useState<Array<any>>([]);
+
+  const populateTable = async () => {
+    if (ticker === null) {
+      return;
+    }
+    const data = await fetchReturns(ticker);
+    setReturns(data.data);
+  };
+  return (
+    <main className="max-w-6xl w-full mx-auto gap-20 mb-24 px-8 py-2 flex flex-col">
+      <section className="flex w-full justify-between items-center">
+        <h1 className="text-4xl font-semibold">{ticker ?? "Enter a Ticker"}</h1>
+        <button
+          className="bg-accent text-bg text-xl px-6 py-2 font-semibold"
+          onClick={populateTable}
+        >
+          Fetch
+        </button>
+      </section>
+      {returns?.length === 0 ? (
+        <h2>Enter a valid ticker to search</h2>
+      ) : (
+        <table className="">
+          <thead>
+            <tr className="py-2 border">
+              <th className="border">Date</th>
+              <th className="border">Open</th>
+              <th className="border">High</th>
+              <th className="border">Low</th>
+              <th className="border">Close</th>
+              <th className="border">Volume</th>
+            </tr>
+          </thead>
+          <tbody>
+            {returns?.map((d) => (
+              <tr key={d.date}>
+                <td>{d.date.split("T")[0]}</td>
+                <td>{Number(d.open).toFixed(2)}</td>
+                <td>{Number(d.high).toFixed(2)}</td>
+                <td>{Number(d.low).toFixed(2)}</td>
+                <td>{Number(d.close).toFixed(2)}</td>
+                <td>{Number(d.volume)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </main>
+  );
+}
