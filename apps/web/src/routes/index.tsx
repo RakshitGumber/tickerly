f2fc00578200e9519dc463a582d5d0e80485b005
RakshitGumber <gumberrakshit@gmail.com>
2026-07-09 22:02:11 +0530

    data now renders on the screen

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,27 +1,23 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import { Navbar } from "../components/Navbar";
 import { fetchReturns } from "../api";
 import { useTicker } from "../store/useTicker";
 
 export default function Home() {
   const { ticker } = useTicker((state) => state);
+  const [returns, setReturns] = useState<Array<any>>([]);
 
   const populateTable = async () => {
     if (ticker === null) {
       return;
     }
     const data = await fetchReturns(ticker);
-    console.log(data);
+    setReturns(data.data);
   };
-
-  useEffect(() => {
-    populateTable();
-  }, []);
-
   return (
     <div className="flex flex-col gap-8">
       <Navbar />
-      <main className="max-w-6xl w-full mx-auto px-8 py-2 flex flex-col">
+      <main className="max-w-6xl w-full mx-auto gap-20 mb-24 px-8 py-2 flex flex-col">
         <section className="flex w-full justify-between items-center">
           <h1 className="text-4xl font-semibold">
             {ticker ?? "Enter a Ticker"}
@@ -33,6 +29,34 @@ export default function Home() {
             Fetch
           </button>
         </section>
+        {returns?.length === 0 ? (
+          <h2>Enter a valid ticker to search</h2>
+        ) : (
+          <table className="">
+            <thead>
+              <tr className="py-2 border">
+                <th className="border">Date</th>
+                <th className="border">Open</th>
+                <th className="border">High</th>
+                <th className="border">Low</th>
+                <th className="border">Close</th>
+                <th className="border">Volume</th>
+              </tr>
+            </thead>
+            <tbody>
+              {returns?.map((d) => (
+                <tr key={d.date}>
+                  <td>{d.date.split("T")[0]}</td>
+                  <td>{Number(d.open).toFixed(2)}</td>
+                  <td>{Number(d.high).toFixed(2)}</td>
+                  <td>{Number(d.low).toFixed(2)}</td>
+                  <td>{Number(d.close).toFixed(2)}</td>
+                  <td>{Number(d.volume)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </main>
     </div>
   );
